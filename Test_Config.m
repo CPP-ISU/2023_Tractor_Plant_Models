7e4bbadc1d5f197ffb5287d8e3336ba9f140fe06
dking1115 <davidking1115@me.com>
2023-04-11 18:24:38 -0500

    Working on structure

--- Test_Config.m
+++ Test_Config.m
@@ -1,12 +1,15 @@
 Front_Tire_Stiffness=2e3;
 Front_Tire_Damping=1e3;
 Front_Tire_Transition_Region=2;
-Back_Tire_Stiffness=5e3;
-Back_Tire_Damping=2.5e3;
+Back_Tire_Stiffness=1e3;
+Back_Tire_Damping=5e2;
 Back_Tire_Transition_Region=2;
 
+Suspension_Hydraulic_Enabled=false;
 Steering_Cylinder_Type=3;
-Suspension_Cylinder_Type=4;
+Suspension_Cylinder_Type=4; %1-Hydraulic, 2-Actuated, 3-Fixed, 4-Spring Damper
+Front_Tire_Collission_Count=1;
+Back_Tire_Collission_Count=1;
 
 Rear_weight_Bracket_Enable=false;
 Rear_weight_Bracket_Weight_Count=25;

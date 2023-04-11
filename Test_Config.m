64d0d3254e1038a8575140cd7b1244dca8a3a3d5
David <davidking1115@me.com>
2023-04-11 01:39:56 -0500

    working on weights and stuff

---
+++ Test_Config.m
@@ -0,0 +1,14 @@
+Front_Tire_Stiffness=2e3;
+Front_Tire_Damping=1e3;
+Front_Tire_Transition_Region=2;
+Back_Tire_Stiffness=5e3;
+Back_Tire_Damping=2.5e3;
+Back_Tire_Transition_Region=2;
+
+Steering_Cylinder_Type=3;
+Suspension_Cylinder_Type=4;
+
+Rear_weight_Bracket_Enable=false;
+Rear_weight_Bracket_Weight_Count=25;
+
+Wheelie_Bar_Enable=true;

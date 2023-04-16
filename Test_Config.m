8605f6179ed4337ca9edb3d7afac86bad10a631e
David <davidking1115@me.com>
2023-04-16 00:00:46 -0500

    got durabilityy working

--- Test_Config.m
+++ Test_Config.m
@@ -4,14 +4,14 @@ Front_Tire_Transition_Region=2;
 Back_Tire_Stiffness=12000;
 Back_Tire_Damping=5e2;
 Back_Tire_Transition_Region=2;
-
+Driver_weight=150;
 Suspension_Hydraulic_Enabled=false;
 Steering_Cylinder_Type=3;
 Suspension_Cylinder_Type=4; %1-Hydraulic, 2-Actuated, 3-Fixed, 4-Spring Damper
 Front_Tire_Collission_Count=4;
 Back_Tire_Collission_Count=4;
 
-Rear_weight_Bracket_Enable=false;
-Rear_weight_Bracket_Weight_Count=25;
+Rear_weight_Bracket_Enable=true;
+Rear_weight_Bracket_Weight_Count=18;
 
 Wheelie_Bar_Enable=true;
